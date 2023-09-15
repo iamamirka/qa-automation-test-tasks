6540612ae0a56f26be7ce25d6b60da43a933851d
iamamir <amir.veston@gmail.com>
2023-09-15 14:48:24 +0800

    refactor: moved the message construction logic into a separate class
    Generalized the message construction logic for all geometric shapes
    Inherited geometrical figures classes from Figure class

--- second_task/geometrical_figures.py
+++ second_task/geometrical_figures.py
@@ -1,29 +1,49 @@
 from color import Color
 from point import Point
 
-class Circle:
+class Figure:
+    def draw():
+        pass
 
-    def __init__(self, center: Point, radius: int) -> None:
+class Circle(Figure):
+
+    def __init__(self, center: Point, radius: int):
         self.center = center
         self.radius = radius
 
     def draw(self, color: Color):
-        print(f"Drawing Circle: {self.center.x, self.center.y} with radius: {self.radius} in {color} color")
+        print(MessageBuilder.build_message(self, color))
 
-class Triangle:
+class Triangle(Figure):
 
-    def __init__(self, points: [Point]) -> None:
+    def __init__(self, points: [Point]):
         self.points = points
 
     def draw(self, color: Color):
-        print(f"Drawing Triangle: with points: {self.points[0]},{self.points[1]},{self.points[2]} in {color} color")
+        print(MessageBuilder.build_message(self, color))
 
-class Rectangle:
+class Rectangle(Figure):
 
-    def __init__(self, upper_left_point: Point, width: int, height: int) -> None:
+    def __init__(self, upper_left_point: Point, width: int, height: int):
         self.upper_left_point = upper_left_point
         self.width = width
         self.height = height
 
     def draw(self, color: Color):
-        print(f"Drawing Rectangle: {self.upper_left_point} with width: {self.width} and height: {self.height} in {color} color")
+        print(MessageBuilder.build_message(self, color))
+
+class MessageBuilder:
+
+    def build_message(figure: Figure, color: Color):
+        figure_name = figure.__class__.__name__
+        figure_parameters = vars(figure)
+        message = f"Drawing {color.name.lower()} {figure_name} with parameters:"
+        for param_name, param_value in figure_parameters.items():
+            message += MessageBuilder._format_parameter(param_name, param_value)
+        return message
+    
+    @staticmethod
+    def _format_parameter(name, value):
+        if isinstance (value, list):
+            return f" {name} " + ", ".join(map(str, value))
+        return f" {name}:{value}"
