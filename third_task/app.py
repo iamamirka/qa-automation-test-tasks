0d057174209d2efa9c43d3a426cde4a43f3741b0
iamamir <amir.veston@gmail.com>
2023-09-17 15:10:04 +0800

    feat: added a method call to app.py

--- third_task/app.py
+++ third_task/app.py
@@ -1,4 +1,17 @@
 from app_initializer import AppInitializer
+from search import BreadthFirstSearch
 
 app_initializer = AppInitializer()
 total_map = app_initializer.initialize()
+
+search = BreadthFirstSearch()
+shortest_path = search.search_for_shortest_distance(total_map)
+
+if shortest_path:
+    print("The shortest path from raft to place of destination is:")
+    for x, y in shortest_path:
+        print(f"({x},{y})", end=" -> ")
+    print("End")
+    print(f"Length of the path: {len(shortest_path)}")
+else:
+    print("No path exists from raft to place of destination.")
