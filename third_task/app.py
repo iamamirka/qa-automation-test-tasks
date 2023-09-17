d42affc2d39f6cf017c1dc6081abbda240f9d7da
iamamir <amir.veston@gmail.com>
2023-09-17 20:20:03 +0800

    fix: display fix

--- third_task/app.py
+++ third_task/app.py
@@ -10,7 +10,7 @@ shortest_path = search.search_for_shortest_distance(total_map)
 if shortest_path:
     print("The shortest path from raft to place of destination is:")
     for x, y in shortest_path:
-        print(f"({x},{y})", end=" -> ")
+        print(f"({x+1},{y+1})", end=" -> ")#Adding 1 because list starts with zero, but users usually count from 1
     print("End")
     print(f"Length of the path: {len(shortest_path)}")
 else:
